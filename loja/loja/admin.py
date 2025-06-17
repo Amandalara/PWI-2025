a55987673a960dc50bc9f6bcc8989987ab0ffe81
Amanda Lara Duarte da Costa <20231011110014@ifrn.local>
2025-06-17 14:14:39 -0300

    Alteração no admin para conseguir adicionar fotos

--- loja/loja/admin.py
+++ loja/loja/admin.py
@@ -6,12 +6,9 @@ class FabricanteAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
-    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
-    'preco', 'categoria',)
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao','preco', 'categoria',)
     empty_value_display = 'Vazio'
     search_fields = ('Produto',)
-    fields = ('Produto', 'destaque', 'promocao',
-'msgPromocao', 'preco', 'categoria',)
 admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
