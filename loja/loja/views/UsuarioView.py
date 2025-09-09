370cd9aa5d8d4ed839fedfc61c9a4b884df12274
Amanda Lara Duarte da Costa <20231011110014@ifrn.local>
2025-09-09 15:54:11 -0300

    Capítulo 9

--- loja/loja/views/UsuarioView.py
+++ loja/loja/views/UsuarioView.py
@@ -1,10 +1,21 @@
+from django.shortcuts import render, get_object_or_404
 from loja.models import Usuario
-from django.shortcuts import render
+from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
 def list_usuario_view(request, id=None):
-# carrega somente usuarios, não inclui os admin
+    # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
     context = {
-    'usuarios': usuarios
+        'usuarios': usuarios
     }
-    return render(request, template_name='usuario/usuario.html', context=context,
-status=200)
+    return render(request, template_name='usuario/usuario.html', context=context, status=200)
+
+def edit_usuario_view(request):
+    print(request.user)
+    usuario = get_object_or_404(Usuario, user=request.user)
+    usuarioForm = UserUsuarioForm(instance=usuario)
+    userForm = UserForm(instance=request.user)
+    context = {
+        'usuarioForm': usuarioForm,
+        'userForm': userForm
+    }
+    return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
